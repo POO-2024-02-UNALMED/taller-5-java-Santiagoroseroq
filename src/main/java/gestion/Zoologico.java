041f06d08e78f767e1bd1dc70e60c906de67724d
Santiagoroseroq <sroseroq@unal.edu.co>
2024-12-10 12:26:12 -0500

    Create Zoologico.java

---
+++ src/main/java/gestion/Zoologico.java
@@ -0,0 +1,57 @@
+
+package gestion;
+
+import java.util.ArrayList;
+import java.util.List;
+import zooAnimales.Animal;
+
+public class Zoologico {
+    private String nombre;
+    private String ubicacion;
+    private List<Zona> zonas;
+
+    public Zoologico() {}
+
+    public Zoologico(String nombre, String ubicacion) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.zonas = new ArrayList<>();
+    }
+
+    public int cantidadTotalAnimales() {
+        int totalAnimales = 0;
+        for (Zona zona : zonas) {
+            totalAnimales += zona.getCantidadAnimales();
+        }
+        return totalAnimales;
+    }
+
+    public void agregarZonas(Zona zona) {
+        this.zonas.add(zona);
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public List<Zona> getZonas() {
+        return zonas;
+    }
+
+    public void setZonas(List<Zona> zonas) {
+        this.zonas = zonas;
+    }
+}
